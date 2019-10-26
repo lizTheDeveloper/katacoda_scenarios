716762ae874680cc541d8f23b428b20b9ccf6e28
Liz Howard <liz@enki.com>
2019-10-26 15:43:47 -0700

    add setup script

--- terraform_and_aws/setup.sh
+++ terraform_and_aws/setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+
+
+yes build-essential zlib1g-dev libssl-dev libncurses-dev libffi-dev libsqlite3-dev libreadline-dev libbz2-dev
+
+
 git clone https://github.com/aws/aws-elastic-beanstalk-cli-setup.git
 ./aws-elastic-beanstalk-cli-setup/scripts/bundled_installer
 
